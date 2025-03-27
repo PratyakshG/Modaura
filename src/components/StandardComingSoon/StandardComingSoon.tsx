bf155d630d336187c0463cf44fefd214ec064098
PratyakshG <pincodeazm@gmail.com>
2025-03-27 17:55:54 +0530

    Fixed Logo Blur

--- src/components/StandardComingSoon/StandardComingSoon.tsx
+++ src/components/StandardComingSoon/StandardComingSoon.tsx
@@ -3,20 +3,21 @@ import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import ComingSoonContent from "../ComingSoonContent/ComingSoonContent";
 import Image from "next/image";
-import logo from "../../../public/ModAura_logo_light.svg";
+import logo from "../../../public/ModAura_logo.png";
 
 export default function StandardComingSoon() {
   return (
     <>
-      <div className="flex flex-col md:flex-row h-dvh w-screen">
+      <div className="flex flex-col md:flex-row h-screen w-screen">
         <div className="w-full h-1/3 md:h-screen flex items-center justify-center">
           <div className="w-3/4 flex flex-col mx-auto text-center md:text-right z-10">
             <Image
               src={logo}
               alt="logo"
-              className=""
             />
-            <h1 className="text-sm md:text-xl font-outfit font-light lowercase">Minimal. Elegant. Effortless.</h1>
+            <h1 className="text-sm md:text-xl font-outfit font-light lowercase">
+              Minimal. Elegant. Effortless.
+            </h1>
           </div>
           <div
             className="w-full md:w-1/2 h-1/3 md:h-full blur-[2px] absolute -z-10"
